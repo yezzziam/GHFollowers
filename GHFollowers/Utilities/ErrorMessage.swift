c8d9139422dee55a785690cbc700aa63155b3ff5
yezzziam <yezzziam@gmail.com>
2020-02-10 17:42:02 +0200

    Error messages handling

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 2/10/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username create an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from th server was invalid. Please try again."
+}
