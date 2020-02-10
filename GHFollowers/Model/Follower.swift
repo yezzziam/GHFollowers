743a0728fcb854af4545e31cbff51570d29bf692
yezzziam <yezzziam@gmail.com>
2020-02-10 15:26:08 +0200

    Created models for User and Follower(s)

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 2/10/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
