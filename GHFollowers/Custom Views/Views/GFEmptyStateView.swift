24bfee2e99e7fcca723dd4bfc63656826f658cd8
yezzziam <yezzziam@gmail.com>
2020-02-13 20:02:12 +0200

    Reordered project files

---
+++ GHFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,21 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 2/13/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
