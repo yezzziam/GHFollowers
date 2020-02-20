2aa5cc00054e1786c8bae7938b5bdfac3d57f427
yezzziam <yezzziam@gmail.com>
2020-02-20 14:29:21 +0200

    Preparing for a User screen
    Added user data request
    Changed User model
    Changed files hirearchy

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,37 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 2/16/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+        
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -12,5 +12,5 @@ enum GFError: String, Error {
     case invalidUsername = "This username create an invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
-    case invalidData = "The data received from th server was invalid. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
 }
