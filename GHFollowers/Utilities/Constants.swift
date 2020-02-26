173b99f412212100ae972153ce9cfd6510ba9245
yezzziam <yezzziam@gmail.com>
2020-02-26 18:37:04 +0200

    Created reusable header

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 2/25/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
