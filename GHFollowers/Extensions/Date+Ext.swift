a441d6b686c9223433f2f8d43e8b37e658bdc291
yezzziam <yezzziam@gmail.com>
2020-03-11 19:50:20 +0200

    Date/String Exts + Registration date in UserInfoVC
    Added Date/String extensions for the Date
    Registration date in UserInfoVC

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Yegor Zubarets on 3/11/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+    
+}
